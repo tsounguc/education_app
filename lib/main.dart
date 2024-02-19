0e5b5672a0f45d14bc559a50317e7df8eeaad351
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-02-19 17:55:54 -0500

    update pubspec.yaml with assets

--- lib/main.dart
+++ lib/main.dart
@@ -7,27 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -38,15 +22,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  
-  
-  
-
-  
-  
-  
-  
 
   final String title;
 
@@ -59,50 +34,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      
-      
-      
-      
-      
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    
-    
-    
-    
-    
-    
     return Scaffold(
       appBar: AppBar(
-        
-        
-        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        
         title: Text(widget.title),
       ),
       body: Center(
-        
-        
         child: Column(
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -119,7 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
