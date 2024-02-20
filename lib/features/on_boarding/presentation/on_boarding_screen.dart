cb4727188c19587ad16ec68fe9a305bdd261fe3a
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-02-19 19:22:11 -0500

    Create OnboardingScreen

---
+++ lib/features/on_boarding/presentation/on_boarding_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+  static const String id = '/onBoardingScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
