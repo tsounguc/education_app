12d8225fafee4971695b83bfeffb3a0c1ea94642
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-02-20 22:17:46 -0500

    Create Failures and Exceptions

---
+++ lib/core/errors/failures.dart
@@ -0,0 +1,21 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  const Failure({required this.message, required this.statusCode});
+
+  final String message;
+  final int statusCode;
+
+  @override
+  List<Object> get props => [message, statusCode];
+}
+
+class CacheFailure extends Failure {
+  const CacheFailure({required super.message, required super.statusCode});
+}
+
+class ServerFailure extends Failure {
+  const ServerFailure({required super.message, required super.statusCode});
+  ServerFailure.fromException(ServerException exception)
+      : this(message: exception.message, statusCode: exception.statusCode);
+}
