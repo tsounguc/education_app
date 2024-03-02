a1e772e3d55d1d74933573ee99123e6b41368c6f
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-03-02 00:23:23 -0500

    Add BlocProvider for OnBoarding

--- lib/core/services/router.dart
+++ lib/core/services/router.dart
@@ -1,13 +1,19 @@
 import 'package:education_app/core/common/screens/page_under_construction.dart';
+import 'package:education_app/core/services/injection_container.dart';
+import 'package:education_app/features/on_boarding/presentation/cubit/on_boarding_cubit.dart';
 import 'package:education_app/features/on_boarding/presentation/views/on_boarding_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   final arguments = settings.arguments;
   switch (settings.name) {
     case OnBoardingScreen.id:
       return _pageBuilder(
-        (context) => const OnBoardingScreen(),
+        (context) => BlocProvider(
+          create: (_) => serviceLocator<OnBoardingCubit>(),
+          child: const OnBoardingScreen(),
+        ),
         settings: settings,
       );
     default:
